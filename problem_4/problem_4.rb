38896ab16ba2a397af04095e87cef1313c863a44
Zach Morek <zmorek@gmail.com>
2012-11-21 21:10:51 -0500

    test with 2 digit numbers

---
+++ problem_4/problem_4.rb
@@ -0,0 +1,36 @@
+=begin
+
+A palindromic number reads the same both ways. The largest palindrome made
+from the product of two 2-digit numbers is 9009 = 91 99.
+
+Find the largest palindrome made from the product of two 3-digit numbers.
+
+=end
+
+=begin
+
+Finding that solution can be done two ways.
+Either we try to factor out a 6 digit number into two 2 digits
+
+Or we try to build palindromes from 2 digit numbers starting at 99 down
+that search space is oddly not that large
+
+=end
+
+def is_palindrome(number)
+  number.to_s.reverse == number.to_s
+end
+
+
+palindromes = {}
+
+for i in 10..99
+  for j in 10..99
+    #puts (i*j).to_s + "=" + i.to_s + "*" + j.to_s
+    if is_palindrome(i*j)
+      palindromes[i*j] = [i,j]
+    end
+  end
+end
+
+puts palindromes.keys.sort
