af0067cf438f9a0b2a97c61273f4511558f3c9e7
Zach Morek <zmorek@gmail.com>
2012-11-21 19:55:58 -0500

    first testing of problem 3

---
+++ problem_3/problem_3.rb
@@ -0,0 +1,30 @@
+# Largest prime factor of 13195 is 29.  Factors are 5, 7, 13, 29
+
+starting_number = 13195
+
+# we need to factor this
+# naiively we can just try every possible number and keep dividing
+# like I did back on a ti83
+# for i in 0 to n
+#   if n%i == 0
+#     n/= i
+# keep it going until you finish
+#
+# I'm already starting to yak shave my way through this
+# what's stopping me... not having specs?  forget em
+#
+#
+
+test_number = starting_number
+divisor = 2
+valid_divisors = []
+
+while divisor <= test_number
+  if test_number % divisor == 0
+    test_number = test_number / divisor
+    valid_divisors.push divisor
+  end
+  divisor += 1
+end
+
+puts valid_divisors
