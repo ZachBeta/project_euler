6054e9f1d0bf1f2067856b43d860abff5c256d74
Zach Morek <zmorek@gmail.com>
2012-11-25 20:31:30 -0500

    fix divisor issue on low numbers

--- problem_23/divisors.rb
+++ problem_23/divisors.rb
@@ -2,7 +2,11 @@ def divisors(n)
   divisors = []
   sqrt = Math.sqrt(n)
 
-  sqrt_loop_end = sqrt.floor + 1
+  if sqrt % 1
+    sqrt_loop_end = sqrt
+  else
+    sqrt_loop_end = sqrt.floor + 1
+  end
   for i in (1..sqrt_loop_end).each do
     if n % i == 0
       divisors << i unless divisors.include?(i)
