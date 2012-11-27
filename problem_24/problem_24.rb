7bca1155c8eaaf6bad187e88e6620671240a2304
Zach Morek <zmorek@gmail.com>
2012-11-27 00:14:20 -0500

    ruby's array has a permutation method... d'oh!

--- problem_24/problem_24.rb
+++ problem_24/problem_24.rb
@@ -19,54 +19,13 @@ or do I... if the algorithm for permuting is clear, the millionth step will be t
 201
 210
 
-a search tree won't help because I need the millionth
-
-012 -> 021
-flip 2nd and 3rd
-
-021 -> 102
-3rd to 1st, 1st to 2nd, 2nd to 3rd
-
-an array will not suffice
-
-3 "objects"
-6 permutations
-
-3*2*1
-
-with 10 characters we have 10*9*8*7*6*5*4*3*2*1
-
-3 628 800 options
-
 =end
 
 #solution... calculate them all, give 1000 000th
-
-require 'pry'
-
 
 digits = []
 (0..2).each do |i|
   digits << i
 end
 
-def inception(permutation_so_far, digits_left, all_permutations)
-  #base case, permutation is finished
-  if digits_left.length == 0
-    return permutation_so_far
-  end
-
-  #or we need to calculate all of them
-  for i in (0..digits_left.length-1)
-    temp_digits_left = digits_left.dup
-    temp_digits_left.delete_at(i)
-    all_permutations << inception(permutation_so_far + [digits_left[i]], temp_digits_left, all_permutations)
-  end
-
-  return all_permutations
-end
-
-permutations = inception([],digits,[])
-puts permutations.inspect
-
-binding.pry
+puts digits.permutation.to_a.inspect
